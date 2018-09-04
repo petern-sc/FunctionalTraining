8bf219c7a587ddb3fb0a49f089f9269e8f160ecb
Peter Nham <peter.nham@rea-group.com>
2018-09-04 22:30:34 +1000

    do folding exercises

--- src/main/scala/com/rea/higherorder/FoldingExercises.scala
+++ src/main/scala/com/rea/higherorder/FoldingExercises.scala
@@ -19,35 +19,70 @@ package com.rea.higherorder
   */
 
 object FoldingExercises {
-
-  def foldLeft[A, B](initialValue: B, list: List[A])(f: (B, A) => B): B = ???
+  // why ()() rather than 3 arguments?
+  def foldLeft[A, B](initialValue: B, list: List[A])(f: (B, A) => B): B = list match {
+    case x :: xs => foldLeft(f(initialValue, x), xs)(f)
+    case _ => initialValue
+  }
 
   /**
    * foldRight is the same as foldLeft, except it processes the list from right to left.
    */
-  def foldRight[A, B](initialValue: B, list: List[A])(f: (A, B) => B): B = ???
+  def foldRight[A, B](initialValue: B, list: List[A])(f: (A, B) => B): B = {
+//    def go(total: List[A], l: List[A]): List[A] = {
+//      l match {
+//        case a :: as => go(a :: total, as)
+//        case _ => total
+//      }
+//    }
+
+    val reversedList = foldLeft(Nil: List[A], list)((acc, n) => n :: acc)
+//    def goFold(initialValue: B, list: List[A])(f: (A,B) => B): B = {
+//      list match {
+//        case x :: xs => goFold(f(x, initialValue), xs)(f)
+//        case Nil => initialValue
+//      }
+//    }
+//
+//    goFold(initialValue, reversedList)(f)
+    foldLeft(initialValue, reversedList){(b,a) => f(a,b)}
+  }
   /**
    * Remember these, from our recursion exercises?  They can all be implemented with either
    * foldLeft or foldRight.
    */
 
-  def sum(x: List[Int]): Int = ???
+  def sum(x: List[Int]): Int = foldLeft(initialValue = 0, list = x)(_ + _)
 
-  def length[A](x: List[A]): Int = ???
+  def length[A](x: List[A]): Int = foldLeft(initialValue = 0, list = x)((acc, _) => acc + 1)
 
   //Careful you'll need a type annotation on the initialValue field
-  def map[A, B](x: List[A])(f: A => B): List[B] = ???
+  // If Nil = List[Nothing], why do we need a type annotation?
+  def map[A, B](x: List[A])(f: A => B): List[B] =
+    foldRight(initialValue = Nil: List[B], list = x)((next, acc) => f(next) :: acc)
 
-  def filter[A](x: List[A], f: A => Boolean): List[A] = ???
+  def filter[A](x: List[A], f: A => Boolean): List[A] =
+    foldRight(initialValue = Nil: List[A], list = x)((next, acc) => {
+      if (f(next)) {next :: acc}
+      else acc
+    })
 
-  def append[A](x: List[A], y: List[A]): List[A] = ???
+  def append[A](x: List[A], y: List[A]): List[A] =
+    foldRight(initialValue = y, list = x)((next, acc) => next :: acc)
 
-  def flatten[A](x: List[List[A]]): List[A] = ???
+  def flatten[A](x: List[List[A]]): List[A] =
+    foldLeft(initialValue = Nil: List[A], list = x)(append)
 
-  def flatMap[A, B](x: List[A], f: A => List[B]): List[B] = ???
+  def flatMap[A, B](x: List[A], f: A => List[B]): List[B] =
+    foldRight(initialValue = Nil: List[B], list = x)((next, acc) => append(f(next), acc))
 
   // Maximum of the empty list is 0
-  def maximum(x: List[Int]): Int = ???
+  def maximum(x: List[Int]): Int =
+    foldLeft(initialValue = 0, list = x){ (next, acc) =>
+      if (next > acc) next
+      else acc
+    }
 
-  def reverse[A](x: List[A]): List[A] = ???
+  def reverse[A](x: List[A]): List[A] =
+    foldLeft(initialValue = Nil: List[A], list = x)((acc, next) => next :: acc)
 }
