570772d61bbffb5ac4d98a03dd19da60aa554e06
Peter Nham <peter.nham@rea-group.com>
2018-10-22 15:28:27 +1100

    IO exercises

--- src/main/scala/com/rea/referentialtransparency/IOExercises.scala
+++ src/main/scala/com/rea/referentialtransparency/IOExercises.scala
@@ -2,15 +2,21 @@ package com.rea.referentialtransparency
 
 import scalaz._, Scalaz._
 import scalaz.effect.IO
+import scala.io.StdIn.readLine
 
 object IOExercises {
 
-  def readLineFromConsole(): IO[String] = ???
+//  sealed trait Done
+//  object Done extends Done
 
-  def writeToConsole(s: String): IO[Unit] = ???
+  def readLineFromConsole(): IO[String] = IO(readLine)
 
-  def readFromConsoleAndWriteToConsole: IO[Unit] = ???
+//  def writeToConsole(s: String): IO[Done] = IO{println(s); Done}
+  def writeToConsole(s: String): IO[Unit] = IO{println(s)}
 
-  def run(effect: IO[Unit]): Unit = ???
+//  def readFromConsoleAndWriteToConsole: IO[Done] = readLineFromConsole().flatMap(writeToConsole)
+  def readFromConsoleAndWriteToConsole: IO[Unit] = readLineFromConsole().flatMap(writeToConsole)
+
+  def run(effect: IO[Unit]): Unit = effect.unsafePerformIO()
 
 }
